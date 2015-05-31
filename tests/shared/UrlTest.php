79af1bbc0d601f33e4135c647982840d875e4c79
Sebastian Heuer <sebastian.heuer@globetrotter.de>
2015-05-31 12:25:53 +0200

    added unit tests for Url

---
+++ tests/shared/UrlTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace TheSeer\Phive {
+
+    class UrlTest extends \PHPUnit_Framework_TestCase {
+
+        /**
+         * @dataProvider invalidUriProvider
+         * @expectedException \InvalidArgumentException
+         *
+         * @param string $invalidUri
+         */
+        public function testThrowsExceptionIfProtocolIsNotHttps($invalidUri) {
+            new Url($invalidUri);
+        }
+
+        public function invalidUriProvider() {
+            return [
+                ['http://example.com'],
+                ['ftp://example.com'],
+                ['example.com']
+            ];
+        }
+
+        public function testCanBeCastToString() {
+            $url = new Url('https://example.com');
+            $this->assertSame('https://example.com', (string)$url);
+        }
+    }
+
+}
+
