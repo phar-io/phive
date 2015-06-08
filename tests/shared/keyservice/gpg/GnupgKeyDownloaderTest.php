7e6b42a2cde9075a8c05387a8bd5231aa757593c
Sebastian Heuer <sebastian@phpeople.de>
2015-06-08 14:40:18 +0200

    fixed unit tests for GnupgKeyDownloader

--- tests/shared/keyservice/gpg/GnupgKeyDownloaderTest.php
+++ tests/shared/keyservice/gpg/GnupgKeyDownloaderTest.php
@@ -11,33 +11,53 @@ namespace TheSeer\Phive {
          */
         private $curl;
 
+        /**
+         * @var LoggerInterface|ObjectProphecy
+         */
+        private $logger;
+
         public function setUp() {
             $this->curl = $this->prophesize(Curl::class);
+            $this->logger = $this->prophesize(LoggerInterface::class);
         }
 
         public function testInvokesCurlWithExpectedParams() {
+            $response = $this->prophesize(CurlResponse::class);
+            $response->getHttpCode()->willReturn(200);
+            $response->getBody()->willReturn('Some Key');
+
             $this->curl->get(
                 'https://example.com/pks/lookup', ['search' => '0x12345678', 'op' => 'get', 'options' => 'mr']
-            )->shouldBeCalled();
+            )->shouldBeCalled()->willReturn($response->reveal());
 
-            $downloader = new GnupgKeyDownloader($this->curl->reveal(), [new Url('https://example.com')]);
+            $downloader = new GnupgKeyDownloader(
+                $this->curl->reveal(), [new Url('https://example.com')], $this->logger->reveal()
+            );
             $downloader->download('12345678');
         }
 
         public function testReturnsExpectedKey() {
-            $this->curl->get(Argument::any(), Argument::any())
-                ->willReturn('Some Key String');
+            $response = $this->prophesize(CurlResponse::class);
+            $response->getHttpCode()->willReturn(200);
+            $response->getBody()->willReturn('Some Key String');
 
-            $downloader = new GnupgKeyDownloader($this->curl->reveal(), [new Url('https://example.com')]);
+            $this->curl->get(Argument::any(), Argument::any())
+                ->willReturn($response->reveal());
+
+            $downloader = new GnupgKeyDownloader(
+                $this->curl->reveal(), [new Url('https://example.com')], $this->logger->reveal()
+            );
             $this->assertSame('Some Key String', $downloader->download('12345678'));
         }
 
         /**
-         * @expectedException \InvalidArgumentException
+         * @expectedException \TheSeer\Phive\CurlException
          */
         public function testThrowsExceptionIfKeyWasNotFound() {
             $this->curl->get(Argument::any(), Argument::any())->willThrow(new CurlException());
-            $downloader = new GnupgKeyDownloader($this->curl->reveal(), [new Url('https://example.com')]);
+            $downloader = new GnupgKeyDownloader(
+                $this->curl->reveal(), [new Url('https://example.com')], $this->logger->reveal()
+            );
             $downloader->download('12345678');
         }
 
