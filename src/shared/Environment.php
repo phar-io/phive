a0ed336f8a63101e7a882ebefabae349ec222cfc
Sebastian Heuer <sebastian@phpeople.de>
2015-06-11 10:37:01 +0200

    added getter for home directory in Environment

--- src/shared/Environment.php
+++ src/shared/Environment.php
@@ -21,8 +21,25 @@ namespace TheSeer\Phive {
         /**
          * @return bool
          */
+        public function hasHomeDirectory() {
+            return array_key_exists('HOME', $this->server);
+        }
+
+        /**
+         * @return string
+         */
+        public function getHomeDirectory() {
+            if (!$this->hasHomeDirectory()) {
+                throw new \BadMethodCallException('No home directory set in environment');
+            }
+            return $this->server['HOME'];
+        }
+
+        /**
+         * @return bool
+         */
         public function hasProxy() {
-            return (array_key_exists('https_proxy', $this->server));
+            return array_key_exists('https_proxy', $this->server);
         }
 
         /**
