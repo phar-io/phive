cc408897e11cd7866852083cf68052bec45780bf
Sebastian Heuer <sebastian@phpeople.de>
2015-09-08 15:54:41 +0200

    added unit tests for Phar

---
+++ tests/shared/PharTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace PharIo\Phive {
+
+    /**
+     * @covers PharIo\Phive\Phar
+     */
+    class PharTest extends \PHPUnit_Framework_TestCase {
+
+        use ScalarTestDataProvider;
+
+        /**
+         * @dataProvider stringProvider
+         *
+         * @param string $name
+         */
+        public function testGetName($name) {
+            $phar = new Phar($name, new Version('1.0.0'), new File('foo.phar', ''));
+            $this->assertSame($name, $phar->getName());
+        }
+
+        /**
+         * @dataProvider versionProvider
+         *
+         * @param Version $version
+         */
+        public function testGetVersion(Version $version) {
+            $phar = new Phar('foo', $version, new File('bar.phar', ''));
+            $this->assertEquals($version, $phar->getVersion());
+        }
+
+        /**
+         * @dataProvider fileProvider
+         *
+         * @param File $file
+         */
+        public function testGetFile(File $file) {
+            $phar = new Phar('foo', new Version('1.0.0'), $file);
+            $this->assertEquals($file, $phar->getFile());
+        }        
+        
+        /**
+         * @return array
+         */
+        public function versionProvider() {
+            return [
+                [new Version('1.0.0')],
+                [new Version('3.5.2')]
+            ];
+        }
+
+        public function fileProvider() {
+            return [
+                [new File('foo.phar', 'bar')],
+                [new File('bar.phar', 'baz')],
+            ];
+        }
+
+    }
+
+}
+
+
