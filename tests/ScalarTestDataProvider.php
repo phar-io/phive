4c2ff2415a5f76c4dacf3978f3209484d67b2f79
Sebastian Heuer <sebastian@phpeople.de>
2015-09-08 15:41:23 +0200

    added test trait with scalar data providers

---
+++ tests/ScalarTestDataProvider.php
@@ -0,0 +1,33 @@
+<?php
+namespace PharIo\Phive {
+
+    trait ScalarTestDataProvider {
+
+        /**
+         * @return array
+         */
+        public function stringProvider() {
+            return [
+                ['foo'],
+                ['bar'],
+                ['äüß'],
+                ['Ӵ', 'Ӹ', 'ה']
+            ];
+        }
+
+        /**
+         * @return array
+         */
+        public function intProvider() {
+            return [
+                [0],
+                [1],
+                [46],
+                [2311341],
+                [-5]
+            ];
+        }
+
+    }
+
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 require __DIR__ . '/../src/autoload.php';
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/ScalarTestDataProvider.php';
 
 if (!extension_loaded('gnupg')) {
     class_alias(\PharIo\Phive\GnuPG::class, '\Gnupg');
