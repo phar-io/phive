ec28623da53918312f59451c93162f0ac80d9878
Sebastian Heuer <sebastian@phpeople.de>
2015-09-08 15:54:59 +0200

    added missing testcase for PharIoRepositoryList

--- tests/shared/PharIoRepositoryListTest.php
+++ tests/shared/PharIoRepositoryListTest.php
@@ -1,27 +1,28 @@
 <?php
 namespace PharIo\Phive {
 
+    /**
+     * @covers PharIo\Phive\PharIoRepositoryList
+     */
     class PharIoRepositoryListTest extends \PHPUnit_Framework_TestCase {
+
+        public function testReturnsEmptyArrayIfFileDoesNotExist() {
+            $list = new PharIoRepositoryList('php://memory/foo.xml');
+            $this->assertEquals([], $list->getRepositoryUrls(new PharAlias('bar')));
+        }
 
         public function testReturnsEmptyArrayForUnknownAlias() {
             $list = new PharIoRepositoryList(__DIR__ . '/../data/repositories.xml');
-
-            $expected = [];
-            $this->assertEquals($expected, $list->getRepositoryUrls(new PharAlias('foo')));
+            $this->assertEquals([], $list->getRepositoryUrls(new PharAlias('foo')));
         }
 
         public function testReturnsExpectedArrayOfUrls() {
             $list = new PharIoRepositoryList(__DIR__ . '/../data/repositories.xml');
 
-            $expected = [
-                new Url('https://phar.phpunit.de'),
-                new Url('https://phar.io')
-            ];
+            $expected = [new Url('https://phar.phpunit.de'), new Url('https://phar.io')];
             $this->assertEquals($expected, $list->getRepositoryUrls(new PharAlias('phpunit')));
 
-            $expected = [
-                new Url('https://phar.io')
-            ];
+            $expected = [new Url('https://phar.io')];
             $this->assertEquals($expected, $list->getRepositoryUrls(new PharAlias('phpab')));
         }
 
