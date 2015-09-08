f3d1d99ba8d242908045211e20a6ebd3f67f7f30
Sebastian Heuer <sebastian@phpeople.de>
2015-09-08 15:42:35 +0200

    added unit tests for KeyImportResult

---
+++ tests/services/key/KeyImportResultTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace PharIo\Phive {
+
+    /**
+     * @covers PharIo\Phive\KeyImportResult
+     */
+    class KeyImportResultTest extends \PHPUnit_Framework_TestCase {
+
+        use ScalarTestDataProvider;
+
+        /**
+         * @dataProvider intProvider
+         *
+         * @param int$count
+         */
+        public function testGetCount($count) {
+            $result = new KeyImportResult($count, null);
+            $this->assertSame($count, $result->getCount());
+        }
+
+        /**
+         * @dataProvider stringProvider
+         *
+         * @param string $fingerprint
+         */
+        public function testGetFingerprint($fingerprint) {
+            $result = new KeyImportResult(1, $fingerprint);
+            $this->assertSame($fingerprint, $result->getFingerprint());
+        }
+
+    }
+
+}
+
+
