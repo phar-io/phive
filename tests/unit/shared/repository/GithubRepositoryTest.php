31bacdee10e8e9720b85e49ad1697c884035c817
Sebastian Heuer <sebastian@phpeople.de>
2016-04-07 18:35:09 +0200

    add unit tests for GithubRepository

---
+++ tests/unit/shared/repository/GithubRepositoryTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace PharIo\Phive;
+
+/**
+ * @covers PharIo\Phive\GithubRepository
+ */
+class GithubRepositoryTest extends \PHPUnit_Framework_TestCase {
+
+    public function testReturnsExpectedReleases() {
+        $entry1 = $this->getGithubEntry('5.3.0', 'https://example.com/foo-5.3.0.phar');
+        $entry2 = $this->getGithubEntry('5.2.11', 'https://example.com/broken');
+        $entry3 = $this->getGithubEntry('5.2.12', 'https://example.com/foo-5.2.12.phar');
+
+        $jsonData = $this->getJsonDataMock();
+        $jsonData->method('getParsed')
+            ->willReturn([$entry1, $entry2, $entry3]);
+
+        $expectedReleases = new ReleaseCollection();
+        $expectedReleases->add(new Release(new Version('5.3.0'), new Url('https://example.com/foo-5.3.0.phar')));
+        $expectedReleases->add(new Release(new Version('5.2.12'), new Url('https://example.com/foo-5.2.12.phar')));
+
+        $repository = new GithubRepository($jsonData);
+        $this->assertEquals(
+            $expectedReleases,
+            $repository->getReleasesByAlias($this->getPharAliasMock())
+        );
+    }
+
+    /**
+     * @param string $version
+     * @param string $url
+     *
+     * @return \stdClass
+     */
+    private function getGithubEntry($version, $url) {
+        $asset = new \stdClass();
+        $asset->browser_download_url = $url;
+
+        $entry = new \stdClass();
+        $entry->tag_name = $version;
+        $entry->assets = [$asset];
+
+        return $entry;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|JsonData
+     */
+    private function getJsonDataMock() {
+        return $this->getMockWithoutInvokingTheOriginalConstructor(JsonData::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|PharAlias
+     */
+    private function getPharAliasMock() {
+        return $this->getMockWithoutInvokingTheOriginalConstructor(PharAlias::class);
+    }
+
+
+}
