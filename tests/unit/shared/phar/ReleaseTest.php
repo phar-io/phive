6bff63accfa81d2a4a1780c59215d9ca04b17412
Sebastian Heuer <sebastian@phpeople.de>
2016-04-07 17:22:36 +0200

    add unit tests for Release

---
+++ tests/unit/shared/phar/ReleaseTest.php
@@ -0,0 +1,59 @@
+<?php
+namespace PharIo\Phive;
+
+/**
+ * @covers PharIo\Phive\Release
+ */
+class ReleaseTest extends \PHPUnit_Framework_TestCase {
+
+    public function testGetVersion() {
+        $version = $this->getVersionMock();
+        $release = new Release($version, $this->getUrlMock());
+
+        $this->assertSame($version, $release->getVersion());
+    }
+
+    public function testGetUrl() {
+        $url = $this->getUrlMock();
+        $release = new Release($this->getVersionMock(), $url);
+
+        $this->assertSame($url, $release->getUrl());
+    }
+
+    public function testGetExpectedHash() {
+        $hash = $this->getHashMock();
+        $release = new Release($this->getVersionMock(), $this->getUrlMock(), $hash);
+
+        $this->assertSame($hash, $release->getExpectedHash());
+    }
+
+    public function testHasExpectedHash() {
+        $release = new Release($this->getVersionMock(), $this->getUrlMock());
+        $this->assertFalse($release->hasExpectedHash());
+
+        $release = new Release($this->getVersionMock(), $this->getUrlMock(), $this->getHashMock());
+        $this->assertTrue($release->hasExpectedHash());
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|Url
+     */
+    private function getUrlMock() {
+        return $this->getMockWithoutInvokingTheOriginalConstructor(Url::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|Version
+     */
+    private function getVersionMock() {
+        return $this->getMockWithoutInvokingTheOriginalConstructor(Version::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|Hash
+     */
+    private function getHashMock() {
+        return $this->getMock(Hash::class);
+    }
+
+}
