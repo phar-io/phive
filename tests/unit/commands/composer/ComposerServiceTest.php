0ab1374b7430c9b7931663c4931fbeef2309b23c
Sebastian Heuer <sebastian@phpeople.de>
2016-04-07 11:11:36 +0200

    add unit tests for ComposerService

---
+++ tests/unit/commands/composer/ComposerServiceTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace PharIo\Phive;
+
+use Prophecy\Prophecy\ObjectProphecy;
+
+/**
+ * @covers PharIo\Phive\ComposerService
+ */
+class ComposerServiceTest extends \PHPUnit_Framework_TestCase {
+
+    public function testFindCandidatesReturnsExpectedPhars() {
+        $filename = new Filename(__DIR__ . '/fixtures/composer.json');
+
+        $sourcesList = $this->getSourcesListProphecy();
+        $sourcesList->getAliasForComposerAlias(new ComposerAlias('phpunit/phpunit'))
+            ->willReturn('phpunit');
+
+        $sourcesList->getAliasForComposerAlias(new ComposerAlias('foo/bar'))
+            ->willThrow(new SourcesListException());
+
+        $sourcesList->getAliasForComposerAlias(new ComposerAlias('theseer/autoload'))
+            ->willReturn('phpab');
+
+        $expectedList = [
+            RequestedPhar::fromAlias(new PharAlias('phpab', new ExactVersionConstraint('1.20.1'))),
+            RequestedPhar::fromAlias(new PharAlias('phpunit', new AnyVersionConstraint())),
+        ];
+
+        $service = new ComposerService($sourcesList->reveal());
+        $this->assertEquals($expectedList, $service->findCandidates($filename));
+    }
+
+    public function testThrowsExceptionIfComposerFileDoesNotExist() {
+        $filename = new Filename(__DIR__ . '/fixtures/foo.json');
+
+        $service = new ComposerService($this->getSourcesListProphecy()->reveal());
+
+        $this->expectException(\InvalidArgumentException::class);
+        $service->findCandidates($filename);
+    }
+
+    /**
+     * @return ObjectProphecy|SourcesList
+     */
+    private function getSourcesListProphecy() {
+        return $this->prophesize(SourcesList::class);
+    }
+
+}
