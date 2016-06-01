1fcca28eda0102ebaab81be1f99bad1b28c3e486
Sebastian Heuer <sebastian@phpeople.de>
2016-06-01 15:56:36 +0200

    add regression test for remove command

--- tests/phar/RemoveCommandTest.php
+++ tests/phar/RemoveCommandTest.php
@@ -2,5 +2,16 @@
 namespace PharIo\Phive\PharRegressionTests;
 
 class RemoveCommandTest extends PharTestCase {
-    
+
+    public function testRemovesSymlink() {
+        $this->addPharToRegistry('phpunit', '5.3.1', 'phpunit-5.3.1.phar');
+
+        file_put_contents(__DIR__ . '/tmp/phpunit', 'foo');
+
+        $this->changeWorkingDirectory(__DIR__ . '/tmp');
+        $this->runPhiveCommand('remove', ['phpunit']);
+
+        $this->assertFileNotExists(__DIR__ .'/tmp/tools/phpunit');
+    }
+
 }
