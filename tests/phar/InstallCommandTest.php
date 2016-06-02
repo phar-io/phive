ecf0f8ff9055294a422f7fb9e6b079d0b23ac687
Sebastian Heuer <sebastian@phpeople.de>
2016-06-02 11:52:26 +0200

    add regression test for install command

--- tests/phar/InstallCommandTest.php
+++ tests/phar/InstallCommandTest.php
@@ -1,6 +1,10 @@
 <?php
 namespace PharIo\Phive\PharRegressionTests;
 
+use PharIo\Phive\ExactVersionConstraint;
+use PharIo\Phive\PharAlias;
+use PharIo\Phive\RequestedPhar;
+
 class InstallCommandTest extends PharTestCase {
 
     public function testInstallsPhar() {
@@ -12,6 +16,25 @@ class InstallCommandTest extends PharTestCase {
             $this->getToolsDirectory()->file('phpunit'),
             $this->getPhiveHomeDirectory()->child('phars')->file('phpunit-5.3.1.phar')->asString()
         );
+    }
+
+    public function testAddsPharNodeToPhiveXmlConfig() {
+        $phiveXmlConfig = $this->getPhiveXmlConfig();
+        $this->addPharToRegistry('phpunit', '5.3.1', 'phpunit-5.3.1.phar');
+
+        $this->assertEmpty($phiveXmlConfig->getPhars());
+
+        $this->runPhiveCommand('install', ['phpunit@5.3.1']);
+
+        // config needs to be reloaded
+        $phiveXmlConfig = $this->getPhiveXmlConfig();
+
+        $expectedPhars = [
+            RequestedPhar::fromAlias(new PharAlias('phpunit', new ExactVersionConstraint('5.3.1')))
+        ];
+
+        $this->assertEquals($expectedPhars, $phiveXmlConfig->getPhars());
+
     }
 
 }
