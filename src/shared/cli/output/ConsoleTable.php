8add73e653b25f2e3b771b8d5993880a51309e27
Sebastian Heuer <sebastian@phpeople.de>
2016-07-15 15:43:25 +0200

    add ConsoleTable

---
+++ src/shared/cli/output/ConsoleTable.php
@@ -0,0 +1,65 @@
+<?php
+namespace PharIo\Phive\Cli;
+
+class ConsoleTable {
+
+    const COLUMN_PADDING = 4;
+
+    /**
+     * @var array
+     */
+    private $headers = [];
+
+    /**
+     * @var array
+     */
+    private $rows = [];
+
+    /**
+     * @param array $headers
+     */
+    public function __construct(array $headers) {
+        $this->headers = $headers;
+    }
+
+    /**
+     * @param array $row
+     */
+    public function addRow(array $row) {
+        $this->rows[] = $row;
+    }
+
+    /**
+     * @return string
+     */
+    public function asString() {
+        $output = '';
+        $mask = '';
+        foreach ($this->headers as $index => $header) {
+            $columnWidth = $this->getColWidth($index);
+            $mask .= "%-{$columnWidth}.{$columnWidth}s";
+        }
+        $mask .= "\n";
+
+        $output .= vsprintf($mask, $this->headers);
+
+        foreach ($this->rows as $row) {
+            $output .= vsprintf($mask, $row);
+        }
+
+        return $output;
+    }
+
+    /**
+     * @param int $index
+     *
+     * @return int
+     */
+    private function getColWidth($index) {
+        $colWidth = strlen($this->headers[$index]);
+        foreach ($this->rows as $row) {
+            $colWidth = max($colWidth, strlen($row[$index]));
+        }
+        return $colWidth + self::COLUMN_PADDING;
+    }
+}
