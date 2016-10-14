f07da5e00fc0ee92d8fca293484e14347ff76e32
Arne Blankerts <Arne@Blankerts.de>
2016-10-14 19:44:12 +0200

    Fixes #82: Adjust timeouts
    Instead of a global timeout, added low speed limit (128b/s for 30seconds)
    and raised connection timeout from 5 to 30 seconds

--- src/shared/http/CurlConfig.php
+++ src/shared/http/CurlConfig.php
@@ -77,17 +77,18 @@ class CurlConfig {
      */
     public function asCurlOptArray() {
         $options = [
-            CURLOPT_MAXREDIRS      => 5,
-            CURLOPT_CONNECTTIMEOUT => 5,
-            CURLOPT_TIMEOUT        => 60,
-            CURLOPT_SSL_VERIFYHOST => 2,
-            CURLOPT_SSL_VERIFYPEER => true,
-            CURLOPT_FAILONERROR    => true,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_USERAGENT      => $this->userAgent,
-            CURLOPT_PROXY          => $this->proxyUrl,
-            CURLOPT_PROXYUSERPWD   => $this->proxyCredentials
+            CURLOPT_MAXREDIRS       => 5,
+            CURLOPT_CONNECTTIMEOUT  => 30,
+            CURLOPT_SSL_VERIFYHOST  => 2,
+            CURLOPT_SSL_VERIFYPEER  => true,
+            CURLOPT_FAILONERROR     => true,
+            CURLOPT_RETURNTRANSFER  => true,
+            CURLOPT_FOLLOWLOCATION  => true,
+            CURLOPT_USERAGENT       => $this->userAgent,
+            CURLOPT_PROXY           => $this->proxyUrl,
+            CURLOPT_PROXYUSERPWD    => $this->proxyCredentials,
+            CURLOPT_LOW_SPEED_TIME  => 30,
+            CURLOPT_LOW_SPEED_LIMIT => 128
         ];
 
         /*
