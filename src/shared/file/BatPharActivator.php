cd9173fa9c026e0e53f172c8502dee81d5f2cc90
Thomas Weinert <thomas@weinert.info>
2018-05-05 18:01:20 +0200

    Improved BatPharActivator: throw an exception if destination is not writable, use variable reference for path if possible

--- src/shared/file/BatPharActivator.php
+++ src/shared/file/BatPharActivator.php
@@ -21,10 +21,19 @@ class BatPharActivator implements PharActivator {
      * @param Filename $linkDestination
      *
      * @return Filename
+     * @throws FileNotWritableException
      */
     public function activate(Filename $pharLocation, Filename $linkDestination) {
-        $template = str_replace(self::PHAR_PLACEHOLDER, $pharLocation->asString(), $this->template);
         $linkFilename = new Filename($linkDestination->asString() . '.bat');
+        if (!$linkDestination->getDirectory()->isWritable()) {
+            throw new FileNotWritableException(sprintf('File %s is not writable.', $linkFilename->asString()));
+        }
+        if ((string)$pharLocation->getDirectory() === (string)$linkFilename->getDirectory()) {
+            $pathToPhar = '%~dp0' . $pharLocation->getRelativePathTo($linkFilename->getDirectory())->asString();
+        } else {
+            $pathToPhar = $pharLocation->asString();
+        }
+        $template = str_replace(self::PHAR_PLACEHOLDER, $pathToPhar, $this->template);
         file_put_contents($linkFilename, $template);
 
         return $linkFilename;
