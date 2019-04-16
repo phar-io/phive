e55acd2fa7b2b071746ee46a148897f60cbf1e6f
Arne Blankerts <Arne@Blankerts.de>
2019-04-17 00:15:26 +0200

    Tweak phpstan config

--- build/phpstan/bootstrap.php
+++ build/phpstan/bootstrap.php
@@ -1,6 +1,8 @@
-<?php
+<?php declare(strict_types = 1);
 
 class Gnupg {
-	public function import($p) {}
-	public function verify($p, $p2) {}
+    public function import($p): array {
+    }
+    public function verify($p, $p2): array {
+    }
 }
