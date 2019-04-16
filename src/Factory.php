deb1adb97d17982d2cbde14890f20977d46aef5b
Arne Blankerts <Arne@Blankerts.de>
2019-04-16 14:33:03 +0200

    Remove useless instanceof

--- src/Factory.php
+++ src/Factory.php
@@ -255,7 +255,7 @@ class Factory {
     }
 
     private function getPhiveVersion(): PhiveVersion {
-        if (!$this->version instanceof PhiveVersion) {
+        if ($this->version === null) {
             $this->version = new GitAwarePhiveVersion($this->getGit());
         }
 
