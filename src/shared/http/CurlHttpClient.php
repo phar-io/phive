ffa6a3b9ec0bce284f78f96bb2ec5fad1d0995ae
Arne Blankerts <Arne@Blankerts.de>
2020-04-12 00:15:26 +0200

    Enhance RateLimit header parsing
    This is now more readable and can deal with gitlab and github's variants
    (e.g. with and without the 'X-'-Prefix) and is fully case-insensitive.

--- src/shared/http/CurlHttpClient.php
+++ src/shared/http/CurlHttpClient.php
@@ -74,13 +74,18 @@ class CurlHttpClient implements HttpClient {
 
     public function handleHeaderInput($ch, string $line): int {
         $parts = \explode(':', \trim($line));
-
-        if (\strtolower($parts[0]) === 'etag') {
-            $this->etag = new ETag(\trim($parts[1]));
+        if (!isset($parts[1])) {
+            return \mb_strlen($line);
         }
 
-        if (\strpos($parts[0], 'X-RateLimit-') !== false) {
-            $this->rateLimitHeaders[\substr($parts[0], 12)] = \trim($parts[1]);
+        [$header, $value] = $parts;
+        $header = \ucfirst(\strtolower($header));
+        $value = \trim($value);
+
+        if ($header === 'Etag') {
+            $this->etag = new ETag($value);
+        } else if (preg_match('/^(X-)?RateLimit-(.*)$/i', $header, $matches) === 1) {
+            $this->rateLimitHeaders[$matches[2]] = $value;
         }
 
         return \mb_strlen($line);
