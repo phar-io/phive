cdc3411c5aaad09f3971fa7acc3491753660e36c
Andreas Möller <am@localheinz.com>
2024-04-03 10:39:42 +0200

    Fix: Case

--- build/phpstan/bootstrap.php
+++ build/phpstan/bootstrap.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
-if (!class_exists('Gnupg')) {
-    class Gnupg {
+if (!class_exists('gnupg')) {
+    class gnupg {
         public function import($p): array {
         }
 
--- src/shared/GnuPG.php
+++ src/shared/GnuPG.php
@@ -14,13 +14,13 @@ use function putenv;
 use PharIo\FileSystem\Directory;
 
 class GnuPG {
-    /** @var \GnuPG */
+    /** @var \gnupg */
     private $gnupg;
 
     /** @var Directory */
     private $homeDir;
 
-    public function __construct(\GnuPG $gnupg, Directory $homeDir) {
+    public function __construct(\gnupg $gnupg, Directory $homeDir) {
         $this->gnupg   = $gnupg;
         $this->homeDir = $homeDir;
     }
